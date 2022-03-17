8f25e4aa63666a0ea5bceff141ed769f45000651
Thomas-Howest <thomas.eeckhout@student.howest.be>
2022-03-17 18:42:14 +0100

    first bits signify length

--- chest.py
+++ chest.py
@@ -1,7 +1,20 @@
 from pathlib import Path
-from typing import Dict, Literal, MutableMapping, Tuple
+from typing import Dict, List, Literal, MutableMapping, Tuple
+from msgspec import DecodeError
 
 from msgspec.msgpack import Decoder, Encoder
+
+
+def lenb(bytes: bytes):
+    return len(bytes).to_bytes(4, "big", signed=False)
+
+
+class ChestException(Exception):
+    "Base class for exceptions during usage of ChestDB."
+
+
+class IndexException(ChestException):
+    "Something went wrong when trying to load the indexfile."
 
 
 class ChestDatabase(MutableMapping):
@@ -16,21 +29,26 @@ class ChestDatabase(MutableMapping):
         self._indexfile: Path = path
         self._datafile: Path = path.with_suffix(".bin")
 
+        # writemode, readonly or read and write.
         self._mode = mode
 
         # The index is an in-memory dict, mirroring the index file.
-        self._index: dict | None = None  # maps keys to (pos, siz) pairs
-        self._free_blocks = []
-        self._modified = False
+        # It maps indexes to their position in the datafile
+        self._index: Dict[str | bytes, int] = dict()
 
-        self._decoder = Decoder(Dict[str | bytes, Tuple[int, int]])
+        # free_blocks keep track of the free blocks in the file.
+        # These free bocks might be reused later on.
+        self._free_blocks: List[int] = []
+
+        # If there has been any change to the index in memory and the index has not been committed, this will be True.
+        self._modified: bool = False
+
+        # initializing an encoder and decoder wich will be used for the index.
+        self._decoder = Decoder(Dict[str | bytes, int])
         self._encoder = Encoder()
 
+        # checks that everything exists
         self._check()
-
-        self.fh_index = self._indexfile.open("rb+", buffering=0)
-        self.fh_data = self._datafile.open("rb+", buffering=0)
-
         self._load()
 
     def _check(self):
@@ -45,119 +63,122 @@ class ChestDatabase(MutableMapping):
         if not self._indexfile.exists():
             if self._mode == "r":
                 raise FileNotFoundError(
-                    f"""File can't be found, use access_mode='r+' if you wan to create it.
-                        Path: <{self._indexfile.absolute()}>,
+                    f"""File can't be found, use access_mode='r+' if you wan to create it.\nPath: <{self._indexfile.absolute()}>,
                         """
                 )
-            # generate the directory if it doesn't exist
-            self._indexfile.parent.mkdir(parents=True, exist_ok=True)
 
-            # create all the files if they don't already exist
+        if self._mode == "r+":
             self._indexfile.touch()
-            self._datafile.touch(exist_ok=True)
-
-        if not self._indexfile.is_file():
-            raise FileNotFoundError(
-                f"""path does not lead to a file: <{self._indexfile.absolute()}>."""
-            )
+            self._datafile.touch()
+            self.fh_index = self._indexfile.open("rb+", buffering=0)
+            self.fh_data = self._datafile.open("rb+", buffering=0)
+        else:
+            self.fh_index = self._indexfile.open("rb", buffering=0)
+            self.fh_data = self._datafile.open("rb", buffering=0)
 
     def _load(self):
         # ?: needs a way to populate free_blocks
 
-        _decode = self._decoder.decode
-        index = dict()
-
         try:
             self.fh_index.seek(0)
-            index = _decode(self.fh_index.read())
-        except Exception as e:
+            self._index = self._decoder.decode(self.fh_index.read())
+
+        except DecodeError:
+
+            self.fh_index.seek(0)
+            if self.fh_index.read():
+                raise IndexException("Index might be corrupt.")
+
             pass
 
-        self._index = index if index else None
+        except OSError:
+            raise IndexException
 
     def _commit(self):
         # ? should commit be copy on write?
 
-        _encode = self._encoder.encode
-        if self._index is None or not self._modified:
+        if not self._modified:
             return
 
         self.fh_index.seek(0)
-        self.fh_index.write(_encode(self._index))
+        self.fh_index.write(self._encoder.encode(self._index))
 
     def _is__open(self):
         # todo implement this somewhere
         return self._index is None
 
-    def _addval(self, key, val):
-        # ? commit after every change or not, maybe use buffer?
+    def _addval(self, key: bytes, val: bytes):
         self._modified = True
 
         self.fh_data.seek(0, 2)
-        pos = int(self.fh_data.tell())
-        size = self.fh_data.write(val)
 
-        self._index[key] = (pos, size)
-        # self._commit()
+        pos = int(self.fh_data.tell())
+        self.fh_data.write(lenb(val) + val)
+
+        self._index[key] = pos
+        self._commit()
 
     def _setval(self, key, val, pos):
-        # ? see comment on _addval
         self._modified = True
 
         self.fh_data.seek(pos)
-        size = self.fh_data.write(val)
+        self.fh_data.write(lenb(val) + val)
 
-        self._index[key] = (pos, size)
+        self._index[key] = pos
         # self._commit()
 
     def __setitem__(self, key, val):
         # todo fix better persistance algorithm
         # ? instead of all the isinstance check, use try catch -> better to ask forgiveness than permission
-        if not self._mode == "r+":
-            raise PermissionError("Storage is _openend as read only")
+        # if not self._mode == "r+":
+        #     raise PermissionError("Storage is _openend as read only")
 
-        if isinstance(key, str):
-            key = key.encode("utf-8")
+        # if isinstance(key, str):
+        #     key = key.encode("utf-8")
 
-        if not isinstance(key, (bytes, bytearray)):
-            raise TypeError("keys must be bytes or strings")
+        # if not isinstance(key, (bytes, bytearray)):
+        #     raise TypeError("keys must be bytes or strings")
 
-        if isinstance(val, str):
-            val = val.encode("utf-8")
+        # if isinstance(val, str):
+        #     val = val.encode("utf-8")
 
-        if not isinstance(val, (bytes, bytearray)):
-            raise TypeError("values must be bytes or strings")
+        # if not isinstance(val, (bytes, bytearray)):
+        #     raise TypeError("values must be bytes or strings")
 
         new_length = len(val)
+        try:
+            if key in self._index:
 
-        if not self._index:
-            self._index = dict()
+                pos = self._index[key]
+                self.fh_data.seek(pos)
+                size = int.from_bytes(self.fh_data.read(4), "big", signed=False)
 
-        if key in (index := self._index):
-            pos, size = index[key]
+                if new_length <= size:
+                    self._setval(key, val, pos)
+                    return
 
-            if new_length <= size:
-                self._setval(key, val, pos)
-                return
+            for i, block in enumerate(self._free_blocks):
+                block_pos = block[0]
+                block_size = block[1]
 
-        for i, block in enumerate(self._free_blocks):
-            block_pos = block[0]
-            block_size = block[1]
+                if new_length == block_size:
+                    self._setval(key, val, block_pos)
+                    del self._free_blocks[i]
+                    return
 
-            if new_length == block_size:
-                self._setval(key, val, block_pos)
-                del self._free_blocks[i]
-                return
+                elif new_length < block_size:
+                    self._setval(key, val, block_pos)
+                    self._free_blocks[i] = (
+                        block_pos + new_length,
+                        block_size - new_length,
+                    )
+                    return
 
-            elif new_length < block_size:
-                self._setval(key, val, block_pos)
-                self._free_blocks[i] = (block_pos + new_length, block_size - new_length)
-                return
-
-        self._addval(key, val)
+            self._addval(key, val)
+        except Exception as e:
+            raise e
 
     def close(self, exc_type, exc_value, exc_traceback):
-        # ? actually close everything
         self._commit()
         self.fh_index.flush()
         self.fh_data.flush()
@@ -166,13 +187,13 @@ class ChestDatabase(MutableMapping):
 
     def __getitem__(self, key):
         # todo handle the errors
-        if isinstance(key, str):
-            key = key.encode("utf-8")
 
-        pos, siz = self._index[key]  # may raise KeyError
+        pos = self._index[key]  # may raise KeyError
 
         self.fh_data.seek(pos)
-        dat = self.fh_data.read(siz)
+        dat = memoryview(self.fh_data.read(512))
+        siz = dat[0:4]
+        dat = dat[4 : int.from_bytes(siz, "big", signed=False) + 4]
         return dat
 
     def __enter__(self):
@@ -191,7 +212,7 @@ class ChestDatabase(MutableMapping):
 
         self._modified = True
 
-        self._setfree(*self._index.pop(key))
+        self._setfree(self._index.pop(key))
 
         self._commit()
 
@@ -202,23 +223,23 @@ class ChestDatabase(MutableMapping):
         if self._index == None or len(self._index) == 0:
             return 0
         try:
-            self.self.fh_index.seek(0)
+            self.fh_index.seek(0)
             with memoryview(self.fh_index.read(5)) as view:
                 match view[0]:
                     case x if (x & 0b11110000) == self.FIXMAP:
                         return x & 0b00001111
                     case self.MAP_16:
                         return int.from_bytes(view[1:3], "big")
-                        # return int.from_bytes(f.read(2), "big", signed=False)
                     case self.MAP_32:
                         return int.from_bytes(view[1:5], "big")
-                        # return int.from_bytes(f.read(4), "big", signed=False)
                     case _:
                         return 0
         except IndexError:
             return 0
 
-    def _setfree(self, start, length):
+        assert False
+
+    def _setfree(self, start):
         # todo test this better, sorting, performance, ...
         for s, l in self._free_blocks:
             if (s + l + 1) == start:
